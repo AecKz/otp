4039935df0ce6b21e266c8f4d18f5099da3befa2
Pablo Jácome A <pablojacomea@gmail.com>
2020-06-03 18:54:31 -0500

    Init Commit con esqueleto básico

---
+++ otp/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from './_helpers/must-match.validator';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'otp';
+  otpForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { } 
+  ngOnInit() {
+    this.otpForm = this.formBuilder.group({
+        title: ['', Validators.required],
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue]
+    }, {
+        validator: MustMatch('password', 'confirmPassword')
+    });
+}
+
+// convenience getter for easy access to form fields
+get f() { return this.otpForm.controls; }
+
+onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.otpForm.invalid) {
+        return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.otpForm.value, null, 4));
+}
+
+onReset() {
+    this.submitted = false;
+    this.otpForm.reset();
+}
+}
